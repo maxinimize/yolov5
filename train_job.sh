7cb1cf2729ad027d1c2bd48f2d345c0006cda9f5
maxinimize <maxinimize@gmail.com>
2025-07-23 23:50:10 -0400

    Updated training shell script

--- train_job.sh
+++ train_job.sh
@@ -10,13 +10,16 @@
 
 # Load necessary modules
 module load python/3.11
+module load StdEnv/2023
 module load gcc/12.3
 module load cuda/12.2
 module load opencv/4.11.0
 
 # activate virtual environment
-bash yolov5_env.sh
 source yolov5_env/bin/activate
+
+# set OpenCV path for cv2
+export PYTHONPATH=/cvmfs/soft.computecanada.ca/easybuild/software/2023/x86-64-v4/CUDA/gcc12/cuda12.2/opencv/4.11.0/lib/python3.11/site-packages:$PYTHONPATH
 
 # train the YOLOv5 model
 python train_adv.py --img 640 --batch 16 --epochs 300 --data coco128.yaml --weights yolov5s.pt --cache
